058278a4377ba87792a07a3800bb2956cc47f3eb
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2025-05-28 17:05:52 -0700

    add controls, clean up, style

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let myp5;
+let sliderValue = 50;
+let abstractnessValue = 50;
 
-function draw() {
-  background(220);
-}
+new p5(function (p) {
+  myp5 = p;
+
+  p.setup = function () {
+    p.createCanvas(400, 400);
+    p.background(220);
+  };
+
+  p.draw = function () {
+    p.background(220);
+    sliderValue = document.getElementById("roomsSlider").value;
+    abstractnessValue = document.getElementById("abstractnessSlider").value;
+    p.text("Number of Rooms: " + sliderValue, 10, 20);
+    p.text("Abstractness: " + abstractnessValue, 10, 40);
+  };
+}, "main");
