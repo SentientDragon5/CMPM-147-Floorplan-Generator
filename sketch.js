b3eb97e8b8ca2dde37812ecc97feed333fd0cdad
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-05-29 19:54:23 -0700

    testing tiles

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
 let myp5;
 let sliderValue = 50;
 let abstractnessValue = 50;
+const TILE_SIZE = 16;
 
 new p5(function (p) {
   myp5 = p;
+  let tileNames = [
+    "background",
+    "bathroom",
+    "carpet",
+    "kitchen",
+    "wall"
+  ]
+  let tileset = {}
+
+  p.preload = function () {
+    tileNames.forEach(t => {
+      tileset[t] = p.loadImage("./assets/tiles/"+t+".png");
+      console.log(t+" loaded");
+    });
+  };
 
   p.setup = function () {
     let canvas = p.createCanvas(400, 400);
@@ -17,5 +33,25 @@ new p5(function (p) {
     abstractnessValue = document.getElementById("abstractnessSlider").value;
     p.text("Number of Rooms: " + sliderValue, 10, 20);
     p.text("Abstractness: " + abstractnessValue, 10, 40);
+    
+    let x = 0;
+    tileNames.forEach(t => {
+      p.placeTile(x,4*TILE_SIZE,t);
+      x += TILE_SIZE;
+    });
+  };
+
+  p.placeTile = function (x, y, tileName) {
+    p.image(tileset[tileName], x, y);
+  };
+
+  p.drawGrid = function (grid) {
+    p.background(128);
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        p.placeTile(i, j, p.random(4) | 0, 13);
+      }
+    }
   };
 });
