7508f1eae80e2fceceeec518cddad616e084610b
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-05-30 18:16:01 -0700

    add seed parameter

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let sliderValue = 50;
 let abstractnessValue = 50;
 const TILE_SIZE = 16;
 
+let seedValue = 0; // default seed
+
 new p5(function (p) {
   myp5 = p;
   let tileNames = [
@@ -25,15 +27,24 @@ new p5(function (p) {
     let canvas = p.createCanvas(400, 400);
     canvas.parent(document.getElementById("canvasContainer"));
     p.background(220);
+    // Set initial seed
+    seedValue = document.getElementById("seed-box").value;
+    p.randomSeed(Number(seedValue));
   };
 
   p.draw = function () {
+    // Update seed if changed
+    let newSeed = document.getElementById("seed-box").value;
+    if (newSeed !== seedValue) {
+      seedValue = newSeed;
+      p.randomSeed(Number(seedValue));
+    }
     p.background(220);
     sliderValue = document.getElementById("roomsSlider").value;
     abstractnessValue = document.getElementById("abstractnessSlider").value;
     p.text("Number of Rooms: " + sliderValue, 10, 20);
     p.text("Abstractness: " + abstractnessValue, 10, 40);
-    
+
     let x = 0;
     tileNames.forEach(t => {
       p.placeTile(x,4*TILE_SIZE,t);
