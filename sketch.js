0ef167527d124ca14063497e3aedcb5a37336386
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-05-30 18:27:57 -0700

    random grid

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ let sliderValue = 50;
 let abstractnessValue = 50;
 const TILE_SIZE = 16;
 
-let seedValue = 0; // default seed
+let seedValue = Math.floor(Math.random() * 10000);
+let grid = [];
 
 new p5(function (p) {
   myp5 = p;
@@ -24,12 +25,12 @@ new p5(function (p) {
   };
 
   p.setup = function () {
+    document.getElementById("seed-box").value = seedValue;
     let canvas = p.createCanvas(400, 400);
     canvas.parent(document.getElementById("canvasContainer"));
     p.background(220);
-    // Set initial seed
-    seedValue = document.getElementById("seed-box").value;
     p.randomSeed(Number(seedValue));
+    grid = p.generateGrid();
   };
 
   p.draw = function () {
@@ -38,24 +39,38 @@ new p5(function (p) {
     if (newSeed !== seedValue) {
       seedValue = newSeed;
       p.randomSeed(Number(seedValue));
+      grid = p.generateGrid();
     }
     p.background(220);
     sliderValue = document.getElementById("roomsSlider").value;
     abstractnessValue = document.getElementById("abstractnessSlider").value;
-    p.text("Number of Rooms: " + sliderValue, 10, 20);
-    p.text("Abstractness: " + abstractnessValue, 10, 40);
+    //p.text("Number of Rooms: " + sliderValue, 10, 20);
+    //p.text("Abstractness: " + abstractnessValue, 10, 40);
 
-    let x = 0;
-    tileNames.forEach(t => {
-      p.placeTile(x,4*TILE_SIZE,t);
-      x += TILE_SIZE;
-    });
+    // Render the grid
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        p.placeTile(i * TILE_SIZE, j * TILE_SIZE, tileNames[grid[i][j]]);
+      }
+    }
   };
 
   p.placeTile = function (x, y, tileName) {
     p.image(tileset[tileName], x, y);
   };
 
+  p.generateGrid = function() {
+    let cols = Math.floor(p.width / TILE_SIZE);
+    let rows = Math.floor(p.height / TILE_SIZE);
+    let arr = [];
+    for (let i = 0; i < cols; i++) {
+      arr[i] = [];
+      for (let j = 0; j < rows; j++) {
+        arr[i][j] = Math.floor(myp5.random(0, 5));
+      }
+    }
+    return arr;
+  };
   p.drawGrid = function (grid) {
     p.background(128);
 
